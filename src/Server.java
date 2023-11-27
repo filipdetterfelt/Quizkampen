b5ae4cc422e2a8ea45dd605677772515511631f5
Joakim Hansen <jocke.hansen@outlook.com>
2023-11-27 11:06:53 +0100

    Started work on protocol usage

--- src/Server.java
+++ src/Server.java
@@ -40,24 +40,11 @@ public class Server extends Thread{
             ObjectOutputStream p2Out = new ObjectOutputStream(p2.sock.getOutputStream());
             ObjectInputStream p2In = new ObjectInputStream(p2.sock.getInputStream())) {
 
-            /*
-            Test utav protocol hanteringen
-            Kommentera bort för att testa enspelarläget
-             */
-
             TestProtocol testProtocol = new TestProtocol();
             testProtocol.setClientCounter(2);
             testProtocol.process(2);
             p1Out.writeObject(testProtocol.process(listOfCategories));
             Object p1InObj, p2InObj = null, outObj;
-
-            /*
-            Test utav protocolhanteringen
-             */
-
-            /*
-            loopen tar in ett object från klienten, och returnerar det till som protocol klassen processar
-             */
 
 
             while ((p1InObj = p1In.readObject()) != null || (p2InObj = p2In.readObject()) != null){
@@ -69,60 +56,11 @@ public class Server extends Thread{
                     e.printStackTrace();
                 }
             }
-
-
-
-
-
-
-            Object tempObject;
-            String tempString;
-            List<Question> tempQuestions = new ArrayList<>();
-            //p1Out.writeObject(listOfCategories);
-
-
-
-            //Undo nedan kommentars block för att testa enspelar läget, och lägg till | ClassNotFoundException
-/*
-            while ((tempObject = p1In.readObject()) != null){
-                if (tempObject instanceof String){
-                    tempString = (String) tempObject;
-                    System.out.println(tempString);
-                    tempQuestions = qm.getQuestions(tempString,1);
-                    p1Out.writeObject(tempQuestions.get(0));
-                } else if (tempObject instanceof Integer) {
-                    int tempPoint = (Integer) tempObject;
-                    if (tempPoint == 1){
-                        System.out.println("Klienten svarade rätt");
-                        p1.addPoints(tempPoint);
-                        System.out.println("Player 1 points: " + p1.getTotalPoints());
-                    } else {
-                        System.out.println("Klienten svarade fel");
-                        System.out.println("Player 1 points: " + p1.getTotalPoints());
-
-                    }
-                    p1Out.writeObject(p1.getTotalPoints());
-                }
-
-            }
-
- */
-
-
-
-
-
-
-
         }
         catch (IOException e){
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-
     }
-
-
-
 }
