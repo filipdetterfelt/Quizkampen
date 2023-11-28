09f2cfa0919fbd6314613e7df16aa9401c962b3b
Jessica Steinkamp <jessica@steinkamp.se>
2023-11-28 09:09:09 +0100

    new branch with reworked program

---
+++ src/NEW/Client.java
@@ -0,0 +1,124 @@
+package NEW;
+
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Client {
+    Socket socket;
+    String username;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    private static int PORT = 55555;
+
+    public Client(Socket socket, String username) throws ClassNotFoundException, IOException {
+        GameWindow g = new GameWindow();
+        this.socket = socket;
+        this.username = username;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+        try
+
+        {
+            out.writeObject(username);
+            g.drawStartScreen(username);
+
+            Object tempObject;
+            String tempString;
+            List<String> tempList = new ArrayList<>();
+            String cat1 = null, cat2 = null;
+
+            while ((tempObject = in.readObject()) != null) {
+                System.out.println("Klient mottagit object: " + tempObject);
+
+                //Om objektet vi tagit emot från servern är en List<>, följ nedan kodblock
+                if (tempObject instanceof List<?>){
+                    tempList.add((String) ((List<?>) tempObject).get(0));
+                    tempList.add((String) ((List<?>) tempObject).get(1));
+                    cat1 = tempList.get(0);
+                    cat2 = tempList.get(1);
+
+                    //Ritar upp kategorifönstret med de mottagna kategorierna som inparametrar
+                    g.drawCategoryScreen(cat1,cat2);
+
+                    //Action listener för kategorierna, skickar vald kategori som sträng till servern
+                    g.category1Btn.addActionListener(e -> {String temp = g.category1Btn.getText();
+                        try {out.writeObject(temp);} catch (IOException ex) {throw new RuntimeException(ex);}
+                    });
+                    g.category2Btn.addActionListener(e -> {String temp = g.category2Btn.getText();
+                        try {out.writeObject(temp);} catch (IOException ex) {throw new RuntimeException(ex);}
+                    });
+
+                    //Om objektet vi tagit emot från servern är en Question, följ nedan kodblock
+                } else if (tempObject instanceof Question) {
+                    Question tempQ = (Question) tempObject;
+                    System.out.println("Client fick fråga: " + tempQ.getQuestion());
+                    //Ritar upp frågeskärmen med frågan som inparameter
+                    g.drawQuestionsScreen(tempQ);
+
+                    /* Action listener för svarsknapparna, skickar tillbaka en int beroende på om
+                       svaret är rätt eller fel. Rätt = 1, Fel = 0 */
+                    SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            g.answer1Btn.addActionListener(e -> {
+                                if (g.checkAnswer(0,tempQ.getCorrectOptionIndex(),g.answer1Btn)){
+                                    try {out.writeObject(1);} catch (IOException ex) {throw new RuntimeException(ex);}
+                                } else {try {out.writeObject(0);} catch (IOException ex) {throw new RuntimeException(ex);}}
+                            });
+                            g.answer2Btn.addActionListener(e -> {
+                                if (g.checkAnswer(1,tempQ.getCorrectOptionIndex(),g.answer2Btn)){
+                                    try {out.writeObject(1);} catch (IOException ex) {throw new RuntimeException(ex);}
+                                } else {try {out.writeObject(0);} catch (IOException ex) {throw new RuntimeException(ex);}}
+                            });
+                            g.answer3Btn.addActionListener(e -> {
+                                if (g.checkAnswer(2,tempQ.getCorrectOptionIndex(),g.answer3Btn)){
+                                    try {out.writeObject(1);} catch (IOException ex) {throw new RuntimeException(ex);}
+                                } else {try {out.writeObject(0);} catch (IOException ex) {throw new RuntimeException(ex);}}
+
+                            });
+                            g.answer4Btn.addActionListener(e -> {
+                                if (g.checkAnswer(3,tempQ.getCorrectOptionIndex(),g.answer4Btn)){
+                                    try {out.writeObject(1);} catch (IOException ex) {throw new RuntimeException(ex);}
+                                } else {try {out.writeObject(0);} catch (IOException ex) {throw new RuntimeException(ex);}}
+                            });
+                        }
+                    });
+                } else if (tempObject instanceof Integer) {
+                    int tempInt = (Integer) tempObject;
+                    System.out.println("Integer mottagen");
+                    g.drawWaitingForOpponentScreen(tempInt);
+                    System.out.println("Test efter draw");
+                }
+            }
+        } catch (EOFException e){
+            System.out.println("Slutet av filen");
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public ObjectOutputStream getOut() {
+        return out;
+    }
+
+    public ObjectInputStream getIn() {
+        return in;
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your username: ");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost", PORT);
+        Client client = new Client(socket,username);
+    }
+}
---
+++ src/NEW/GameWindow.java
@@ -0,0 +1,231 @@
+package NEW;
+
+import QuestionManager.Question;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+public class GameWindow extends JFrame {
+    //test
+    //startScreen
+    JPanel startScreenPanel = new JPanel();
+    JLabel waitingForOpponentLabel = new JLabel("Väntar på att en motståndare ska koppla upp sig...");
+
+    //categoryScreen
+    JPanel categoryScreenPanel = new JPanel(new GridLayout(3,1,10,10));
+    JPanel categoryCenteringPanel = new JPanel(new GridBagLayout());
+    JLabel categoryLabel = new JLabel("Välj en kategori");
+    JButton category1Btn = new JButton("Kategori 1");
+    JButton category2Btn = new JButton("Kategori 1");
+
+    //questionsScreen
+    JPanel questionsScreenPanel = new JPanel(new GridLayout(2,1,10,10));
+    JPanel questionCenteringPanel = new JPanel(new GridBagLayout());
+    JPanel questionsPanel = new JPanel(new GridLayout(2,2,10,10));
+    JLabel questionLabel = new JLabel("Frågan visas här");
+    JButton answer1Btn = new JButton("Svar 1");
+    JButton answer2Btn = new JButton("Svar 2");
+    JButton answer3Btn = new JButton("Svar 3");
+    JButton answer4Btn = new JButton("Svar 4");
+
+    //waitingForOpponentScreen
+    JPanel waitingForOpponentPanel = new JPanel(new GridLayout(2,1,10,10));
+    JPanel waitingPanel = new JPanel(new GridBagLayout());
+    JLabel waitingForOpponentAnswerLabel = new JLabel("Väntar på att motståndaren ska spela..");
+
+    //resultPanel
+    JPanel scorePanel = new JPanel(new GridLayout(1,2,10,10));
+    JPanel playerCenterPanel = new JPanel(new GridBagLayout());
+    JPanel playerScorePanel = new JPanel(new GridLayout(2,1,10,10));
+    JPanel opponentScorePanel = new JPanel(new GridLayout(2,1,10,10));
+    JPanel opponentCenterPanel = new JPanel(new GridBagLayout());
+    JLabel playerLabel = new JLabel("Dina poäng:");
+    JLabel playerScoreLabel = new JLabel("2");
+    JLabel opponentLabel = new JLabel("Motståndarens poäng:");
+    JLabel opponentScoreLabel = new JLabel("0");
+
+    String userInput = null;
+    public GameWindow(String userInput){this.userInput = userInput;}
+    public GameWindow(){
+
+    }
+
+
+    public void drawStartScreen(String username){
+        add(startScreenPanel);
+        startScreenPanel.setLayout(new GridBagLayout());
+        waitingForOpponentLabel.setText("Välkommen " + username + ".");
+        startScreenPanel.add(waitingForOpponentLabel);
+
+        setTitle("Quizkampen");
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public void drawCategoryScreen(String category1, String category2){
+        clearFrame(startScreenPanel);
+        clearFrame(questionsScreenPanel);
+
+        add(categoryScreenPanel);
+        categoryScreenPanel.add(categoryCenteringPanel);
+        categoryCenteringPanel.add(categoryLabel);
+        categoryScreenPanel.add(category1Btn);
+        categoryScreenPanel.add(category2Btn);
+
+        //Ge knapparna varsin kategori
+        category1Btn.setText(category1);
+        category2Btn.setText(category2);
+
+        //Ritar upp frågeskärmen med den frågan som servern valt
+        category1Btn.addActionListener(e -> {
+            System.out.println(category1);;
+        });
+        category2Btn.addActionListener(e -> {
+            System.out.println(category2);
+        });
+        revalidate();
+        repaint();
+        category1Btn.setFocusable(false);
+        category2Btn.setFocusable(false);
+        categoryScreenPanel.setBorder(BorderFactory.createEmptyBorder(40,20,20,20));
+
+        setTitle("Quizkampen");
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+
+    }
+
+    public void drawQuestionsScreen(Question recievedQuestion){
+        System.out.println("Drawing questionsScreen");
+        clearFrame(categoryScreenPanel);
+        clearFrame(startScreenPanel);
+        clearFrame(waitingForOpponentPanel);
+        resetBackgrounds();
+        add(questionsScreenPanel);
+        questionsScreenPanel.add(questionCenteringPanel);
+        questionCenteringPanel.add(questionLabel);
+        questionsScreenPanel.add(questionsPanel);
+        questionsPanel.add(answer1Btn);
+        questionsPanel.add(answer2Btn);
+        questionsPanel.add(answer3Btn);
+        questionsPanel.add(answer4Btn);
+        revalidate();
+        repaint();
+
+        //Hämta & lagra svarets index från frågan
+        int correctAnswer = recievedQuestion.getCorrectOptionIndex();
+
+        //Hämta & sätt ut frågan och svarsalternativen från frågan
+        questionLabel.setText(recievedQuestion.getQuestion());
+        answer1Btn.setText(Arrays.asList(recievedQuestion.getOptions()).get(0));
+        answer2Btn.setText(Arrays.asList(recievedQuestion.getOptions()).get(1));
+        answer3Btn.setText(Arrays.asList(recievedQuestion.getOptions()).get(2));
+        answer4Btn.setText(Arrays.asList(recievedQuestion.getOptions()).get(3));
+
+        questionsScreenPanel.setBorder(BorderFactory.createEmptyBorder(40,20,20,20));
+        setTitle("Quizkampen");
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+        setResizable(true);
+
+    }
+
+    public void drawWaitingForOpponentScreen(int tempInt){
+        clearFrame(questionsScreenPanel);
+
+        add(waitingForOpponentPanel);
+        waitingForOpponentPanel.add(scorePanel);
+        waitingForOpponentPanel.add(waitingPanel);
+        waitingPanel.add(waitingForOpponentAnswerLabel);
+        scorePanel.add(playerCenterPanel);
+        playerCenterPanel.add(playerScorePanel);
+        scorePanel.add(opponentCenterPanel);
+        opponentCenterPanel.add(opponentScorePanel);
+        playerScorePanel.add(playerLabel);
+        playerScorePanel.add(playerScoreLabel);
+        opponentScorePanel.add(opponentLabel);
+        opponentScorePanel.add(opponentScoreLabel);
+
+        playerScoreLabel.setText(String.valueOf(tempInt));
+        revalidate();
+        repaint();
+        setTitle("Quizkampen");
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public void drawResultScreen(int playerPoints, int opponentPoints){
+        add(scorePanel);
+        scorePanel.add(playerCenterPanel);
+        playerCenterPanel.add(playerScorePanel);
+        playerScorePanel.add(playerLabel);
+        playerScorePanel.add(playerScoreLabel);
+        scorePanel.add(opponentCenterPanel);
+        opponentCenterPanel.add(opponentScorePanel);
+        opponentScorePanel.add(opponentLabel);
+        opponentScorePanel.add(opponentScoreLabel);
+
+        revalidate();
+        repaint();
+        setTitle("Quizkampen");
+        setSize(400,600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+    }
+
+    //Jämför knappens index mot det korrekta svarets index i frågan
+    public boolean checkAnswer(int answeredIndex, int correctIndex, JButton button){
+        if (answeredIndex == correctIndex){
+            button.setBackground(Color.GREEN);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void resetBackgrounds(){
+        answer1Btn.setBackground(Color.GRAY);
+        answer2Btn.setBackground(Color.GRAY);
+        answer3Btn.setBackground(Color.GRAY);
+        answer4Btn.setBackground(Color.GRAY);
+    }
+    private void clearFrame(JPanel panel){
+        this.remove(panel);
+        this.validate();
+        this.repaint();
+    }
+
+    public static void main(String[] args) {
+        //GameWindow gameWindow = new GameWindow();
+        Question testQuestion = new Question("Vilket år inträffade den franska revolutionen?", new String[]{"1789", "1815", "1871", "1799"}, 0);
+        //QuestionDatabase database = new QuestionDatabase();
+        String testCategory1 = "Historia";
+        String testCategory2 = "Sport";
+
+        //gameWindow.drawStartScreen();
+        //gameWindow.drawCategoryScreen(testCategory1, testCategory2);
+        //gameWindow.drawWaitingForOpponentScreen(5);
+        //gameWindow.drawResultScreen();
+
+        //gameWindow.drawQuestionsScreen(testQuestion);
+
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+}
