744625b8026b2005a62c3b07fd4e1df7fbca48e3
Gustav Söderberg <gustav.soderberg@outlook.com>
2023-11-30 16:47:08 +0100

    added better comments + structure

--- src/QuestionManager/QuestionDatabase.java
+++ src/QuestionManager/QuestionDatabase.java
@@ -1,6 +1,7 @@
 package QuestionManager;
 
 import java.util.*;
+
 /**
     QuestionDatabase-class
     QuestionDatabase contains a HashMap with key-value pair: categories(key) -> List<Question>(value)
@@ -23,179 +24,189 @@ public class QuestionDatabase
     private void init()
     {
         // Kategori: Film och underhållning
-        this.addQuestion("film och underhållning", new Question("Vem spelar rollen som Tony Stark/Iron Man i Marvel Cinematic Universe?",
-                new String[]{"Robert Downey Jr.", "Chris Hemsworth", "Chris Evans", "Mark Ruffalo"}, 0));
+        this.addQuestion("film och underhållning", new Question("Iron Man-skådespelare?",
+                new String[]{"Downey Jr.", "Hemsworth", "Evans", "Ruffalo"}, 0));
 
-        this.addQuestion("film och underhållning", new Question("Vem regisserade filmen 'Inception'?",
-                new String[]{"Steven Spielberg", "Christopher Nolan", "Quentin Tarantino", "Martin Scorsese"}, 1));
+        this.addQuestion("film och underhållning", new Question("Inception-regissör?",
+                new String[]{"Spielberg", "Nolan", "Tarantino", "Scorsese"}, 1));
 
-        this.addQuestion("film och underhållning", new Question("Vem regisserade filmen 'Pulp Fiction'?",
-                new String[]{"Steven Spielberg", "Christopher Nolan", "Quentin Tarantino", "Martin Scorsese"}, 2));
+        this.addQuestion("film och underhållning", new Question("Pulp Fiction-regissör?",
+                new String[]{"Spielberg", "Nolan", "Tarantino", "Scorsese"}, 2));
 
-        this.addQuestion("film och underhållning", new Question("Vem spelade rollen som Neo i filmen 'The Matrix'?",
-                new String[]{"Brad Pitt", "Tom Cruise", "Keanu Reeves", "Leonardo DiCaprio"}, 2));
+        this.addQuestion("film och underhållning", new Question("Neo i 'The Matrix'?",
+                new String[]{"Pitt", "Cruise", "Reeves", "DiCaprio"}, 2));
 
-        this.addQuestion("film och underhållning", new Question("Vilken film vann Oscar för Bästa Film år 2021?",
-                new String[]{"The Trial of the Chicago 7", "Minari", "Mank", "Nomadland"}, 3));
+        this.addQuestion("film och underhållning", new Question("Bästa film vid Oscar 2021?",
+                new String[]{"Chicago 7", "Minari", "Mank", "Nomadland"}, 3));
+
 
         // Kategori: Sport
-        this.addQuestion("sport", new Question("Vilket lag vann fotbolls-VM år 2018?",
+        this.addQuestion("sport", new Question("Fotbolls-VM 2018-vinnare?",
                 new String[]{"Kroatien", "Argentina", "Frankrike", "Brasilien"}, 2));
 
-        this.addQuestion("sport", new Question("Vem vann herrsingeln i Wimbledon 2021?",
-                new String[]{"Roger Federer", "Rafael Nadal", "Andy Murray", "Novak Djokovic"}, 3));
+        this.addQuestion("sport", new Question("Wimbledon 2021 herrsingel-vinnare?",
+                new String[]{"Federer", "Nadal", "Murray", "Djokovic"}, 3));
 
-        this.addQuestion("sport", new Question("Vem vann guld i herrarnas singel i tennis vid OS 2020?",
-                new String[]{"Novak Djokovic", "Rafael Nadal", "Roger Federer", "Alexander Zverev"}, 3));
+        this.addQuestion("sport", new Question("OS 2020 herrsingel tennis-guld?",
+                new String[]{"Djokovic", "Nadal", "Federer", "Zverev"}, 3));
 
-        this.addQuestion("sport", new Question("Vilket lag vann Super Bowl LV (55) år 2021?",
-                new String[]{"Kansas City Chiefs", "Tampa Bay Buccaneers", "New England Patriots", "Los Angeles Rams"}, 1));
+        this.addQuestion("sport", new Question("Super Bowl LV (55)-vinnare 2021?",
+                new String[]{"Chiefs", "Buccaneers", "Patriots", "Rams"}, 1));
 
-        this.addQuestion("sport", new Question("Vilket land vann flest medaljer totalt vid OS 2020 i Tokyo?",
+        this.addQuestion("sport", new Question("Flest OS 2020 medaljer?",
                 new String[]{"USA", "Kina", "Japan", "Storbritannien"}, 0));
 
+
         // Kategori: Musik
-        this.addQuestion("musik", new Question("Vem sjunger låten 'Someone Like You'?",
+        this.addQuestion("musik", new Question("'Someone Like You'-sångare?",
                 new String[]{"Adele", "Taylor Swift", "Beyoncé", "Rihanna"}, 0));
 
-        this.addQuestion("musik", new Question("Vilket band hade albumet 'The Dark Side of the Moon'?",
+        this.addQuestion("musik", new Question("'The Dark Side of the Moon'-album?",
                 new String[]{"The Beatles", "Pink Floyd", "Led Zeppelin", "Queen"}, 1));
 
-        this.addQuestion("musik", new Question("Vem var huvudsångaren i bandet Queen?",
+        this.addQuestion("musik", new Question("Queen-huvudsångare?",
                 new String[]{"Freddie Mercury", "John Lennon", "David Bowie", "Mick Jagger"}, 0));
 
-        this.addQuestion("musik", new Question("Vilken sångerska är känd för låten 'Hello'?",
+        this.addQuestion("musik", new Question("'Hello'-sångerska?",
                 new String[]{"Adele", "Taylor Swift", "Beyoncé", "Rihanna"}, 0));
 
-        this.addQuestion("musik", new Question("Vilket instrument spelade Jimi Hendrix mest känd för att spela?",
+        this.addQuestion("musik", new Question("Hendrix instrument?",
                 new String[]{"Bas", "Trummor", "Gitarr", "Piano"}, 2));
 
+
         // Kategori: Vetenskap och natur
-        this.addQuestion("vetenskap och natur", new Question("Vilken planet är närmast solen?",
+        this.addQuestion("vetenskap och natur", new Question("Närmaste planet till solen?",
                 new String[]{"Merkurius", "Venus", "Jorden", "Mars"}, 0));
 
-        this.addQuestion("vetenskap och natur", new Question("Vad kallas det område inom biologin som studerar växtriket?",
+        this.addQuestion("vetenskap och natur", new Question("Studium av växtriket?",
                 new String[]{"Botanik", "Zoologi", "Geologi", "Ekologi"}, 0));
 
-        this.addQuestion("vetenskap och natur", new Question("Vilket element har kemiskt tecken 'H'?",
+        this.addQuestion("vetenskap och natur", new Question("Kemiskt tecken för 'H'?",
                 new String[]{"Helium", "Kväve", "Väte", "Kol"}, 2));
 
-        this.addQuestion("vetenskap och natur", new Question("Vilket djur klassificeras som ett däggdjur och kan flyga?",
+        this.addQuestion("vetenskap och natur", new Question("Flygande däggdjur?",
                 new String[]{"Orangutang", "Papegoja", "Fladdermus", "Krokodil"}, 2));
 
-        this.addQuestion("vetenskap och natur", new Question("Vad är den kemiska formeln för vatten?",
+        this.addQuestion("vetenskap och natur", new Question("Vatten kemiska formel?",
                 new String[]{"H2O2", "CO2", "H2O", "CH4"}, 2));
 
+
         // Kategori: Historia
-        this.addQuestion("historia", new Question("Vilket år inträffade den amerikanska självständighetsförklaringen?",
+        this.addQuestion("historia", new Question("Amerikanska självständighetsförklaringen?",
                 new String[]{"1776", "1789", "1812", "1848"}, 0));
 
-        this.addQuestion("historia", new Question("Vilken person var den första kvinnan att flyga över Atlanten?",
+        this.addQuestion("historia", new Question("Första kvinnan över Atlanten?",
                 new String[]{"Amelia Earhart", "Beryl Markham", "Valentina Tereshkova", "Sally Ride"}, 0));
 
-        this.addQuestion("historia", new Question("Vilket år inträffade den franska revolutionen?",
+        this.addQuestion("historia", new Question("Franska revolutionens år?",
                 new String[]{"1789", "1815", "1871", "1799"}, 0));
 
-        this.addQuestion("historia", new Question("Vilket år bröt första världskriget ut?",
+        this.addQuestion("historia", new Question("Första världskrigets utbrott?",
                 new String[]{"1914", "1917", "1919", "1918"}, 0));
 
-        this.addQuestion("historia", new Question("Vilket år skedde den amerikanska inbördeskriget?",
+        this.addQuestion("historia", new Question("Amerikanska inbördeskrigets år?",
                 new String[]{"1861", "1850", "1870", "1865"}, 0));
 
+
         // Kategori: Teknologi
-        this.addQuestion("teknologi", new Question("Vilket år grundades OpenAI?",
+        this.addQuestion("teknologi", new Question("OpenAI-grundningsår?",
                 new String[]{"2017", "2015", "2016", "2014"}, 0));
 
-        this.addQuestion("teknologi", new Question("Vilket år lanserades den första iPhone?",
+        this.addQuestion("teknologi", new Question("Första iPhone-lanseringsår?",
                 new String[]{"2006", "2007", "2008", "2005"}, 1));
 
-        this.addQuestion("teknologi", new Question("Vilket företag skapade det första operativsystemet Windows?",
+        this.addQuestion("teknologi", new Question("Första Windows skapat av vilket företag?",
                 new String[]{"Apple", "Microsoft", "IBM", "Google"}, 1));
 
-        this.addQuestion("teknologi", new Question("Vilken teknologigrundare grundade företaget SpaceX?",
+        this.addQuestion("teknologi", new Question("SpaceX grundare?",
                 new String[]{"Jeff Bezos", "Elon Musk", "Mark Zuckerberg", "Larry Page"}, 1));
 
-        this.addQuestion("teknologi", new Question("Vilket år släpptes det första PlayStation-konsolen?",
+        this.addQuestion("teknologi", new Question("Första PlayStation lanseringsår?",
                 new String[]{"1992", "1994", "1996", "1998"}, 1));
 
+
         // Kategori: Böcker
-        this.addQuestion("böcker", new Question("Vem skrev boken '1984'?",
+        this.addQuestion("böcker", new Question("'1984'-författare?",
                 new String[]{"George Orwell", "Aldous Huxley", "Ray Bradbury", "H.G. Wells"}, 0));
 
-        this.addQuestion("böcker", new Question("Vilken författare skapade karaktären Sherlock Holmes?",
+        this.addQuestion("böcker", new Question("Skapare av Sherlock Holmes?",
                 new String[]{"Charles Dickens", "Agatha Christie", "Arthur Conan Doyle", "Edgar Allan Poe"}, 2));
 
-        this.addQuestion("böcker", new Question("Vilken bok är känd för att vara J.R.R. Tolkiens första del i trilogin 'The Lord of the Rings'?",
+        this.addQuestion("böcker", new Question("Första delen i 'The Lord of the Rings'?",
                 new String[]{"The Two Towers", "The Return of the King", "The Fellowship of the Ring", "The Hobbit"}, 2));
 
-        this.addQuestion("böcker", new Question("Vilken författare är känd för boken 'Pride and Prejudice'?",
+        this.addQuestion("böcker", new Question("'Pride and Prejudice'-författare?",
                 new String[]{"Emily Brontë", "Jane Austen", "Charlotte Brontë", "Mary Shelley"}, 1));
 
-        this.addQuestion("böcker", new Question("Vilken författare är känd för att ha skrivit boken 'To Kill a Mockingbird'?",
+        this.addQuestion("böcker", new Question("'To Kill a Mockingbird'-författare?",
                 new String[]{"John Steinbeck", "Harper Lee", "F. Scott Fitzgerald", "Mark Twain"}, 1));
 
+
         // Kategori: Geografi
-        this.addQuestion("geografi", new Question("Vad är huvudstaden i Sverige?",
+        this.addQuestion("geografi", new Question("Sveriges huvudstad?",
                 new String[]{"Stockholm", "Oslo", "Helsingfors", "Köpenhamn"}, 0));
 
-        this.addQuestion("geografi", new Question("Vilket land är världens största till ytan?",
+        this.addQuestion("geografi", new Question("Största landet till ytan?",
                 new String[]{"Ryssland", "Kanada", "Kina", "USA"}, 0));
 
-        this.addQuestion("geografi", new Question("Vilken flod är världens längsta?",
+        this.addQuestion("geografi", new Question("Världens längsta flod?",
                 new String[]{"Amazonfloden", "Nilen", "Mississippifloden", "Yangtzefloden"}, 1));
 
-        this.addQuestion("geografi", new Question("Vilket land har Eiffeltornet?",
+        this.addQuestion("geografi", new Question("Land med Eiffeltornet?",
                 new String[]{"Italien", "Spanien", "Frankrike", "Tyskland"}, 2));
 
-        this.addQuestion("geografi", new Question("Vilket hav är det största i världen?",
+        this.addQuestion("geografi", new Question("Största havet i världen?",
                 new String[]{"Södra Ishavet", "Atlanten", "Stilla Havet", "Indiska Oceanen"}, 2));
 
-        // Kategori: Programmering(Java)
-        this.addQuestion("programmering", new Question("Vad är skillnaden mellan '==' och '.equals()' i Java för att jämföra strängar?",
-                new String[]{"De gör samma sak", "==' används för att jämföra objektens referenser medan '.equals()' jämför objektens innehåll", "Det finns ingen skillnad", "'.equals()' används för att jämföra objektens referenser medan '==' jämför objektens innehåll"}, 1));
 
-        this.addQuestion("programmering", new Question("Vilket nyckelord används för att ärva en klass i Java?",
+        // Kategori: Programmering(Java)
+        this.addQuestion("programmering", new Question("'==' vs '.equals()' för strängjämförelse?",
+                new String[]{"Samma", "Referenser", "Ingen", "Innehåll"}, 1));
+
+        this.addQuestion("programmering", new Question("Arvnyckelord i Java?",
                 new String[]{"extends", "implements", "inherit", "inherits"}, 0));
 
-        this.addQuestion("programmering", new Question("Vilket är det grundläggande syntaxen för en for-loop i Java?",
-                new String[]{"for (int i = 0; i < 5; i++) {}", "loop (int i = 0; i < 5; i++) {}", "for (i = 0; i < 5) {}", "repeat (int i = 0; i < 5; i++) {}"}, 0));
+        this.addQuestion("programmering", new Question("For-loop syntax i Java?",
+                new String[]{"for", "loop", "repeat", "while"}, 0));
 
-        this.addQuestion("programmering", new Question("Vad är skillnaden mellan en 'Array' och en 'ArrayList' i Java?",
-                new String[]{"Ingen skillnad", "En 'Array' är dynamisk medan en 'ArrayList' är statisk", "En 'Array' är statisk medan en 'ArrayList' är dynamisk", "En 'Array' är enkel att använda medan en 'ArrayList' är komplicerad"}, 2));
+        this.addQuestion("programmering", new Question("Skillnad mellan 'Array' och 'ArrayList'?",
+                new String[]{"Ingen", "Dynamisk", "Statisk", "Enkel"}, 2));
 
-        this.addQuestion("programmering", new Question("Vad betyder 'Java' i Java-programmeringsspråket?",
-                new String[]{"En förkortning av 'JavaScript Virtual Application'", "En referens till kaffebönan", "En förkortning av 'Just Another Very Advanced'", "Det har ingen specifik betydelse"}, 1));
+        this.addQuestion("programmering", new Question("Betydelse av 'Java' i Java-språket?",
+                new String[]{"Förkortning", "Kaffe", "Avancerad", "Ingen"}, 1));
 
-        this.addQuestion("programmering", new Question("Vilken typ av loop används för att iterera över element i en samling?",
+        this.addQuestion("programmering", new Question("Loop för samlingar?",
                 new String[]{"for", "while", "foreach", "if"}, 2));
 
-        this.addQuestion("programmering", new Question("Vad kallas processen att omdefiniera en metod i en subklass?",
+        this.addQuestion("programmering", new Question("Metod-omdefiniering i subklass?",
                 new String[]{"Överskuggning", "Överbelastning", "Nedärvning", "Abstraktion"}, 0));
 
-        this.addQuestion("programmering", new Question("Vad används 'int', 'String', och 'boolean' som exempel på?",
+        this.addQuestion("programmering", new Question("Exempel på 'int', 'String', 'boolean'?",
                 new String[]{"Datatyper", "Metoder", "Variabler", "Klasser"}, 0));
 
-        this.addQuestion("programmering", new Question("Vad innebär begreppet 'null' i Java?",
-                new String[]{"Ett ogiltigt värde", "En odefinierad variabel", "Ett tomt objekt", "En typ av loop"}, 2));
+        this.addQuestion("programmering", new Question("Betydelsen av 'null' i Java?",
+                new String[]{"Ogiltigt", "Odefinierad", "Tomt", "Loop"}, 2));
 
-        this.addQuestion("programmering", new Question("Vilket nyckelord används för att undvika att ett objekt kan ärvas?",
+        this.addQuestion("programmering", new Question("Arvundvikande nyckelord?",
                 new String[]{"final", "static", "abstract", "private"}, 0));
 
-        this.addQuestion("astronomi", new Question("Vilken planet är närmast solen i vårt solsystem?",
-                new String[]{"Mars", "Jupiter", "Venus", "Jorden"}, 2));
 
         //Kategori: Astronomi
-        this.addQuestion("astronomi", new Question("Vad kallas den fas av månen när hela dess yta är upplyst?",
+        this.addQuestion("astronomi", new Question("Närmaste planet till solen?",
+                new String[]{"Mars", "Jupiter", "Venus", "Jorden"}, 2));
+
+        this.addQuestion("astronomi", new Question("Fas av månen när hela ytan är upplyst?",
                 new String[]{"Nymåne", "Halvmåne", "Fullmåne", "Månförmörkelse"}, 2));
 
-        this.addQuestion("astronomi", new Question("Vad är en samling av stjärnor som hålls ihop av gravitationen?",
+        this.addQuestion("astronomi", new Question("Samling av stjärnor som hålls ihop av gravitationen?",
                 new String[]{"Planetsystem", "Galax", "Komet", "Nebulosa"}, 1));
 
-        this.addQuestion("astronomi", new Question("Vad är den ljusstarka punkten vid centrum av de flesta galaxer?",
-                new String[]{"Svart hål", "Komet", "Supernova", "Galaxkärna"}, 3));
+        this.addQuestion("astronomi", new Question("Ljusstark punkt vid galaxers centrum?",
+                new String[]{"Svart hål", "Komet", "Supernova", "Galaxkärna"}, 0));
 
-        this.addQuestion("astronomi", new Question("Vad kallas det fenomen där en mindre himlakropp passerar framför en större, skymmer dess ljus och skapar en skugga på jorden?",
+        this.addQuestion("astronomi", new Question("Fenomen där mindre himlakropp passerar framför en större och skapar en skugga på jorden?",
                 new String[]{"Asteroidbana", "Månförmörkelse", "Solsken", "Ockultation"}, 3));
+
     }
 
     //Method for adding the question, if the key already exists in the HashMap (the category name) it'll add the Question-object to the list, otherwise creates a new key for the category
